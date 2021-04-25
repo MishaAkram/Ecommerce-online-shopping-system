811a03a79f1be4d15733e5d6ebbf46ee0e336a41
MishaAkram <mishaakrambaig@gmail.com>
2021-04-25 12:46:23 +0500

    ui components added

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Modal.scss';
+
+// import Backdrop from '../Backdrop/Backdrop';
+
+const Modal = ({ showBackdrop, closeModal, modalType, showModal, children }) => (
+  <>
+    {/* <Backdrop show={showBackdrop} clicked={closeModal} /> */}
+    <div
+      className={["modal", modalType].join(' ')}
+      style={{
+        transform: showModal ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: showModal ? '1' : '0'
+      }}>
+      {children}
+    </div>
+  </>
+)
+
+export default Modal;
