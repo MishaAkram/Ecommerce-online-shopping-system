387a24d485f4a7963ee2aa7dc2de966a67dbf9fe
Iqra-Irfan <74137053+Iqra-Irfan@users.noreply.github.com>
2021-05-02 17:26:06 +0500

    HomePage code converted to functional component

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -6,13 +6,34 @@ import slideTwo from '../../assets/home_page/slide_2.jpg';
 import slideThree from '../../assets/home_page/slide_3.jpg';
 import slideFour from '../../assets/home_page/slide_4.jpg';
 import slideFive from '../../assets/home_page/slide_5.jpg';
+import { useSelector, useDispatch } from 'react-redux';
+import Button from '../../components/UI/Button/Button';
+import Modal from '../../components/UI/Modal/Modal';
+import PropTypes from 'prop-types';
+import { purchaseInit } from '../../store/actions';
 
 const slides = [];
 slides.push(slideOne, slideTwo, slideThree, slideFour, slideFive);
 
 function HomePage() {
+    const purchased = useSelector(state => state.order.purchased)
+    const dispatch = useDispatch();
+
+    const closeModal = () => {
+        dispatch(purchaseInit())
+        dispatch(closeModal())
+    };
+
     return (
         <div>
+            <Modal
+                modalType="small"
+                showModal={purchased}
+                showBackdrop={purchased}
+                closeModal={closeModal}>
+                <p>Order completed successfully.</p>
+                <Button clicked={closeModal} btnType="dark">Got it</Button>
+            </Modal>
             <div className="home-container">
                 <div className="showcase">
                 </div>
@@ -28,5 +49,11 @@ function HomePage() {
         </div>
     )
 }
+
+HomePage.propTypes = {
+    purchased: PropTypes.bool.isRequired,
+    purchaseInit: PropTypes.func.isRequired,
+    closeModal: PropTypes.func.isRequired
+};
 
 export default HomePage
