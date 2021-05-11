35c543f641476552a10096693855d71e14f9adf6
Iqra-Irfan <74137053+Iqra-Irfan@users.noreply.github.com>
2021-05-11 23:13:19 +0500

    wishlist code converted to functional component

--- src/containers/Wishlist/Wishlist.js
+++ src/containers/Wishlist/Wishlist.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Wishlist.scss';
-import { connect } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import * as actions from '../../store/actions';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
 import CSSTransition from 'react-transition-group/CSSTransition';
@@ -10,12 +10,15 @@ import WishlistItem from './WishlistItem/WishlistItem';
 import Button from '../../components/UI/Button/Button';
 import ScrollToTopOnMount from '../../shared/ScrollToTopOnMount';
 
-const wishlist = (props) => {
+function Wishlist() {
+  const wishlistItems = useSelector( state => state.products.wishlist)
+  const dispatch = useDispatch()
+  // const wishlist = (props) => {
   let list;
-  (props.wishlistItems.length === 0) ? list = <p className="main-info" style={{ marginTop: '20px', fontWeight: '500' }}>You do not have any products on the list yet.</p> :
+  (wishlistItems.length === 0) ? list = <p className="main-info" style={{ marginTop: '20px', fontWeight: '500' }}>You do not have any products on the list yet.</p> :
     list = (
       <TransitionGroup component="ul" className="wishlist-list">
-        {props.wishlistItems.map(item => (
+        {wishlistItems.map(item => (
           <CSSTransition key={item.id} classNames="fade" timeout={300}>
             <WishlistItem item={item} />
           </CSSTransition>
@@ -30,29 +33,30 @@ const wishlist = (props) => {
         <h2 className="main-title">Wishlist</h2>
         <p className="main-info">Lorem ipsum dolor adipiscing elit. Donec eleifend ligula neque, at faucibus metus trum sedru.</p>
         {list}
-        {props.wishlistItems.length > 0 &&
-          <Button clicked={props.clearWishlist} btnType="dark">Clear Wishlist</Button>
+        {wishlistItems.length > 0 &&
+          <Button clicked={dispatch(actions.clearWishlist())} btnType="dark">Clear Wishlist</Button>
         }
       </div>
     </>
   )
-};
+  // };
+}
 
-wishlist.propTypes = {
+Wishlist.propTypes = {
   wishlistItems: PropTypes.array.isRequired,
   clearWishlist: PropTypes.func.isRequired
 };
 
-const mapStateToProps = state => {
-  return {
-    wishlistItems: state.products.wishlist
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     wishlistItems: state.products.wishlist
+//   };
+// };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    clearWishlist: () => dispatch(actions.clearWishlist())
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     clearWishlist: () => dispatch(actions.clearWishlist())
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(wishlist);
+export default Wishlist;
