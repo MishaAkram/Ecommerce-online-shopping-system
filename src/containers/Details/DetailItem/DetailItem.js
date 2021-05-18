16151710dea7af424feccd41d3cbe3c5d843932d
MishaAkram <mishaakrambaig@gmail.com>
2021-05-18 23:04:17 +0500

    Details Page ui uodate

--- src/containers/Details/DetailItem/DetailItem.js
+++ src/containers/Details/DetailItem/DetailItem.js
@@ -1,10 +1,21 @@
 import React, { } from 'react';
 import './DetailItem.scss';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
-function DetailItem({ children}) {
+import { Divider } from '@material-ui/core';
+const thumb2 = {
+  display: 'inline-flex',
+  borderRadius: 0,
+  border: '3px solid #eaeaea',
+  marginBottom: 2,
+  marginRight: 2,
+  width: 345,
+  height: 500,
+  padding: 5,
+  boxSizing: 'border-box'
+};
+function DetailItem({ children }) {
   const detailProduct = useSelector(state => state.products.detailProduct)
   if (detailProduct === null) return <Redirect to="/" />;
   const { title, subtitle, img, description, price } = detailProduct;
@@ -14,12 +25,13 @@ function DetailItem({ children}) {
       <p className="detail-value">{subtitle}</p>
       <div className="detail-content">
         <div className="detail-img-wrapper">
-          <img src={img} alt="" className="detail-item-img" />
+          <img src={img} alt="" className="detail-item-img" style={thumb2} />
         </div>
         <div className="detail-info">
           <h3 className="detail-subtitle">Description:</h3>
           <p className="detail-value">{description}</p>
           <h3 className="detail-subtitle">Price: {price}.00 $</h3>
+          <Divider/>
           {children}
         </div>
       </div>
