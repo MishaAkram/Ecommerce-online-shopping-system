30fc3a612ed0ddb453464677d3cfb6b6869edad8
MishaAkram <mishaakrambaig@gmail.com>
2021-05-31 01:06:31 +0500

    wishlist link error resolved

--- src/containers/Wishlist/WishlistItem/WishlistItem.js
+++ src/containers/Wishlist/WishlistItem/WishlistItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './WishlistItem.scss';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Button from '../../../components/UI/Button/Button';
 import { showDetails, removeWishlistItem } from './../../../store/actions/productActions';
 
 const WishlistItem = ({ item }) => {
-  const { id, title, subtitle, img, description, price } = item
+  const { id, title, subtitle, img, description, price } = item;
   const dispatch = useDispatch();
+  const history =useHistory();
   return (
     <li className="wishlist-item">
       <h3 className="wishlist-title">{title}</h3>
@@ -20,11 +21,15 @@ const WishlistItem = ({ item }) => {
         <div className="wishlist-info">
           <h3 className="wishlist-subtitle">Description:</h3>
           <p className="wishlist-value">{description}</p>
-          <h3 className="wishlist-subtitle">Price: {price}.00 $</h3>
+          <h3 className="wishlist-subtitle">PKR {price}</h3>
           <div className="btn-wrapper">
-            <Link to={`/details/${id}`}>
-              <Button clicked={(id) => dispatch(showDetails(id))}>Show Details</Button>
-            </Link>
+                <Button
+                 clicked={() =>
+                  history.push({
+                    pathname: "/details/" + id,
+                    data: item
+                  })
+                  }>Show Details</Button>
             <Button clicked={(id) => dispatch(removeWishlistItem(id))} btnType="dark">Remove</Button>
           </div>
         </div>
