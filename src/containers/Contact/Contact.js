21883ceea535e1f1a3b9bdce982d2712a5dea59e
MishaAkram <mishaakrambaig@gmail.com>
2021-06-01 23:31:46 +0500

    contact page css changes

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,78 +1,66 @@
 import React from 'react';
 import './Contact.scss';
 import ScrollToTopOnMount from '../../shared/ScrollToTopOnMount';
-
+import { Divider } from '@material-ui/core';
 const contact = () => (
   <>
     <ScrollToTopOnMount />
     <div className="contact-container">
-      <h2 className="main-title">Contact</h2>
-      <p className="main-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend ligula neque, at faucibus metus rutrum sed. Fusce interdum at est eget aliquet. Suspendisse potenti. Curabitur ac luctus magna.  Donec eleifend ligula neque, at faucibus metus rutrum sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <h3 className="title">Phone number:</h3>
-      <p>555-444-333 (free connection)</p>
-      <h3 className="title">Work hours:</h3>
-      <p>Monday - Friday: 9.00 - 20.00</p>
-      <p>Saturday - SUnday: 10.00 - 16.00</p>      
-        <div className="col-sm-6 col-xs-12 ">
-          <p>
-            <strong>Let us know what you think.</strong>
-          </p>
-          <p>
-            <span style={{ fontSize: "small" }}>Should you have any question, comments or inquiries please reach out to us.</span>
-          </p>
-          <h4>FOR ONLINE STORE</h4>
-          <p>
-            <strong>BY EMAIL</strong>
-            <br />
-            <a href="mailto:eshop@junaidjamshed.com">
-              <span style={{ fontSize: "small" }}>eshop@junaidjamshed.com</span>
-            </a>
-          </p>
-          <p>
-            <strong>BY TELEPHONE</strong>
-            <br />
-            <span style={{ fontSize: "small" }}>
-              <span style={{ color: "#0000ff" }}>
-                <a href="tel:021111112111">
-                  <span style={{ color: "#0000ff" }}>021 111 112 111</span>
-                </a>
-              </span> (9am - 10pm , Monday - Saturday, Sunday 11am - 08pm )</span>
-          </p>
-          <p>
-            <strong>BY MAIL</strong>
-            <br />
-            <span style={{ fontSize: "small" }}>J. Junaid Jamshed Pvt Ltd</span>
-            <br />
-            <span style={{ fontSize: "small" }}>Head office, 40 Sector 19korangi industrial Area</span>
-          </p>
-          <h4>FOR OUTLETS INQUIRIES</h4>
-          <p>
-            <strong>BY EMAIL</strong>
-            <br />
-            <span style={{ fontSize: "small" }}>feedback@junaidjamshed.com</span>
-          </p>
-          <p>
-            <strong>BY TELEPHONE</strong>
-            <br />
-            <span style={{ fontSize: "small" }}>
-              <span style={{ color: "#0000ff" }}>
-                <a href="tel:021 111 112 111">
-                  <span style={{ color: "#0000ff" }}>021 111 112 111</span>
-                </a>
-              </span> (9am - 6pm , Monday - Saturday )</span>
-          </p>
-          <p>
-            <strong>IN STORE LOCATIONS &amp; PHONE NUMBERS</strong>
-            <br />
-            <span style={{ fontSize: "small" }}>For complete store details, <a href="https://www.junaidjamshed.com/store-locations">click here</a></span></p>
-          <h4>FOR CORPORATE ORDERS AND INQUIRIES</h4>
-          <p>
-            <strong>BY EMAIL</strong>
-            <br />
-            <span style={{ fontSize: "small" }}>Corporate@junaidjamshed.com</span>
-          </p>
-          <style>
-          </style>
+      <h2 className="main-title" style={{ fontFamily: "ACourier New, monospace" }}>Contact</h2>
+      <Divider/>
+      <p className="main-info"></p>
+      <h3 className="title" style={{ fontFamily: "ACourier New, monospace" }}>Phone number:</h3>
+      <p style={{ fontFamily: "ACourier New, monospace" }}>555-444-333 (free connection)</p>
+      <h3 className="title" style={{ fontFamily: "ACourier New, monospace" }}>Work hours:</h3>
+      <p style={{ fontFamily: "ACourier New, monospace" }}>Monday - Friday: 9.00 - 20.00</p>
+      <p style={{ fontFamily: "ACourier New, monospace" }}>Saturday - SUnday: 10.00 - 16.00</p>
+      <div className="col-sm-6 col-xs-12 ">
+        <p style={{ fontFamily: "ACourier New, monospace" }}>
+          <strong style={{ fontFamily: "ACourier New, monospace" }}>Let us know what you think.</strong>
+        </p>
+        <p>
+          <span style={{ fontSize: "small", fontFamily: "ACourier New, monospace" }}>Should you have any question, comments or inquiries please reach out to us.</span>
+        </p>
+        <h4 style={{ fontFamily: "ACourier New, monospace" }}>FOR ONLINE STORE</h4>
+        <p>
+          <strong style={{ fontFamily: "ACourier New, monospace" }}>BY EMAIL</strong>
+          <br />
+          <a href="mailto:eshop@junaidjamshed.com">
+            <span style={{ fontSize: "small", fontFamily: "ACourier New, monospace" }}>eshop@junaidjamshed.com</span>
+          </a>
+        </p>
+        <p>
+          <strong style={{ fontFamily: "ACourier New, monospace" }}>BY TELEPHONE</strong>
+          <br />
+          <span style={{ fontSize: "small", fontFamily: "ACourier New, monospace" }}>
+            <span style={{ color: "#0000ff", fontFamily: "ACourier New, monospace" }}>
+              <a href="">
+                <span style={{ color: "#0000ff", fontFamily: "ACourier New, monospace" }}>021 111 112 111</span>
+              </a>
+            </span> (9am - 10pm , Monday - Saturday, Sunday 11am - 08pm )</span>
+        </p>
+
+        <p>
+          <strong style={{ fontFamily: "ACourier New, monospace" }}>BY TELEPHONE</strong>
+          <br />
+          <span style={{ fontSize: "small", fontFamily: "ACourier New, monospace" }}>
+            <span style={{ color: "#0000ff", fontFamily: "ACourier New, monospace" }}>
+              <a href="">
+                <span style={{ color: "#0000ff" }}>021 111 112 111</span>
+              </a>
+              </span>
+              </span>
+              </p>
+              <p>
+                </p>
+              <h4 style={{ fontFamily: "ACourier New, monospace" }}>FOR CORPORATE ORDERS AND INQUIRIES</h4>
+              <p>
+                <strong style={{ fontFamily: "ACourier New, monospace" }}>BY EMAIL</strong>
+                <br />
+                <span style={{ fontSize: "small" }}>Corporate@elegantStore.com</span>
+              </p>
+              <style>
+              </style>
         </div>
         </div>
   </>
